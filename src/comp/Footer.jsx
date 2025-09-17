527f961fd4eff1fcc0cf4193772f056d8cdeac21
Ahmed Nour Eldeen <ahmednour312314@gmail.com>
2025-09-18 02:24:15 +0300

    Update Footer.jsx

--- src/comp/Footer.jsx
+++ src/comp/Footer.jsx
@@ -1,44 +1,14 @@
 import React from "react";
-import "./Footer.css";
-
-import { useTranslation } from "react-i18next";
+import    './Footer.css';
 
 const Footer = () => {
-  const {  i18n } = useTranslation();
-
-  if (i18n.language === "ar") {
-    return (
-      <div className="myfooter">
-        <footer dir="rtl" className="ali   ">
-          {/*        */}
-      تم التصميم والبرمجة بواسطة احمد نور الدين
-        
-        </footer>
-      </div>
-    );
-  }
-
-  if (i18n.language === "en") {
-    return (
-      <div className="myfooter">
-        <footer className="ahmed">
-          Designed and developed by Ahmed Nour Eldeen
-        
-        </footer>
-      </div>
-    );
-  }
-
-  if (i18n.language === "fr") {
-    return (
-      <div className="myfooter">
-        <footer className="ali   ">
-          Conçu et développé par Ahmed Nour Eldeen
-        
-        </footer>
-      </div>
-    );
-  }
+  return (
+<div className="myfooter">
+      <footer className="ali   ">
+        Designed and developed by Ahmed Nour Eldeen
+      </footer>
+</div>
+  );
 };
 
 export default Footer;
